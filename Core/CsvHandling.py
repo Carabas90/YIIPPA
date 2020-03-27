8fbf7528f99bb1e6247a2296118f54c8e89f603b
Andreas Wagner <AndreasW.90@gmx.de>
2020-03-27 10:54:35 +0100

    Initial Commit

---
+++ Core/CsvHandling.py
@@ -0,0 +1,73 @@
+import csv
+import datetime
+import random
+import os
+import shutil
+
+
+def create_new(first_name, last_name, birthdate):
+    """
+    Creates a new CSV-File with the standard header and adds an entry in legend.csv. The filename 
+    is bates on dtetime.now() with a random int thrown in as a last digit, to avoid collisions if
+    application is ever used in a large scale setting.
+    """
+    filename = datetime.datetime.now().strftime(format='%Y%m%d%H%M%S')+str(random.randint(0,9))+'.csv'
+    p = os.path.join(os.curdir , 'PatientData/', filename)
+    with open(p, 'w', newline='\n') as f:
+        writer = csv.writer(f)
+        header = ['datetime', 'insulin_dose', 'blood_glucose' ]
+        writer.writerow(header)
+    
+    p = os.path.join(os.curdir, 'PatientData/', 'legend.csv')
+    
+    if os.path.exists(p):
+        with open(p, 'a', newline='\n') as l:
+            writer = csv.writer(l)
+            data = [first_name, last_name, birthdate, filename]
+            writer.writerow(data)
+    else:
+        with open(p, 'w', newline='\n') as l:
+            writer = csv.writer(l)
+            header = ['first_name', 'last_name', 'birthdate', 'filename']
+            writer.writerow(header)
+            data = [first_name, last_name, birthdate, filename]
+            writer.writerow(data) 
+    
+
+def import_data(filename):
+    """
+    Imports the Data of a csv file as a nested list, including the header.
+    """
+    path = os.path.join(os.curdir, 'PatientData/', filename)
+    with open(path, 'r', newline='\n') as f:
+        reader = csv.reader(f, delimiter=',')
+        data = []
+        for r in reader:
+            data.append(r)
+        return data
+
+
+def archive_patient(first_name, last_name, birthdate):
+    legend = import_data('legend.csv')
+    for s in legend:
+        if s[0]==first_name and s[1]==last_name and s[2]==birthdate:
+            filename = s[3]
+            i = legend.index(s)
+    
+    src = os.path.join(os.curdir, 'PatientData/', filename)
+    dest = os.path.join(os.curdir, 'Archive/', filename)
+    shutil.move(src, dest)
+    
+    del legend[i]
+    path = os.path.join(os.curdir, 'PatientData/', 'legend.csv')
+    with open(path, 'w', newline='\n') as l:
+        writer = csv.writer(l)
+        for p in legend:
+            writer.writerow(p)
+
+
+    
+
+create_new('Rike', 'Müller', '20.03.1991')
+create_new('Andreas', 'Wagner', '05.06.1990')
+archive_patient('John', 'Doe', '01.01.2000')
